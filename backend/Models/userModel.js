87896f47a5dd89d629469875aef0442531280c87
Ketan222 <ketanmodgil@gmail.com>
2025-02-04 20:56:48 +0530

    feat: jwt token on login and signup

--- backend/Models/userModel.js
+++ backend/Models/userModel.js
@@ -76,9 +76,16 @@ userSchema.pre("save", async function(next){
     this.passwordChangedAt = Date.now();
     next();
 })
+userSchema.pre(/^find/, function(next){
+    // filter out all the active users only
+    this.find({active: true});
+
+    next();
+});
 
 userSchema.methods.correctPassword = async function(candidatePassword, userPassword){
     return await bcrypt.compare(candidatePassword, userPassword);
 }
+
 const User = mongoose.model("User", userSchema);
 module.exports =  User;
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,9 @@
 const User = require('../Models/userModel.js');
+const jwt = require('jsonwebtoken');
 
+const signToken = function(id){
+    return jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRES_IN});
+}
 
 exports.signup = async function(req, res, next){
     const newUser = await User.create({
@@ -8,10 +12,17 @@ exports.signup = async function(req, res, next){
         password: req.body.password,
         passwordConfirm: req.body.passwordConfirm,
     });
+    const token = signToken(newUser._id);
+    const cookieOptions = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN),
+        httpOnly: true,
+    }
+    res.cookie('jwt', token, cookieOptions);
     // console.log(req.body.name, req.body.email);
     // console.log("signup");
     res.status(200).json({
         status: 'success',
+        // token,
         data:{
             newUser,
         }
@@ -22,6 +33,17 @@ exports.login = async function(req, res, next){
     if(!email || !password) res.status(400).json({status:'Enter email && password'});
     const user = await User.findOne({email: email}).select('+password');
     if(!user || !(await user.correctPassword(password, user.password))) res.status(401).json({status: "enter valid email and password"});
+    // console.log(user.id);
+    const id = user.id;
+    const token = jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+    })
+    const cookieOptions = {
+        expires: new Date(Date.now() + (process.env.JWT_COOKIE_EXPIRES_IN*1)),
+        httpOnly: true,
+    }
+    // console.log(token);
+    res.cookie('jwt', token, cookieOptions);
     res.status(200).json({
         status: 'login successfull',
         data:{
